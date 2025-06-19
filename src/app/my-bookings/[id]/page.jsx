999ce44f664e151efd1032bb93a77eaac89aab5d
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-06-19 18:12:46 +0600

    successfully deploy vercel

--- src/app/my-bookings/[id]/page.jsx
+++ src/app/my-bookings/[id]/page.jsx
@@ -7,7 +7,7 @@ export default async function UpdateBookingPage({params}) {
 
 	const p = await params ;
 	const res = await fetch(`${process.env.NEXTAUTH_URL}/api/my-bookings/${p.id}`, {
-		headers: await headers(),
+		headers: new Headers(await headers()),
 	});
 	const data = await res.json();
 
